90673e9ecaf389efb633c33027f53f482e91707e
clancy <a7mdmo2mn@gmail.com>
2026-01-12 17:01:40 +0200

    Partial validation for updating fields based on what's passed

--- AuthApp/Features/User/Validators/UpdateProfileValidator.cs
+++ AuthApp/Features/User/Validators/UpdateProfileValidator.cs
@@ -8,6 +8,35 @@ public class UpdateProfileValidator : AbstractValidator<UpdateProfileDto>
 {
     public UpdateProfileValidator()
     {
-        RuleFor(x => x.Bio).NotEmpty().WithMessage("Bio is required");
+        RuleFor(x => x).Must(HasAtLeastOneField).WithMessage("At least one field must be provided");
+
+        When(
+            x => x.Bio is not null,
+            () =>
+            {
+                RuleFor(x => x.Bio).NotEmpty().WithMessage("Bio cannot be empty");
+            }
+        );
+
+        //  When(x => x.xxx is not null, () =>
+        // {
+        //     RuleFor(x => x.xxx)
+        //         .NotEmpty()
+        //         .WithMessage("Name cannot be empty");
+        // });
+
+        // When(x => x.yyy is not null, () =>
+        // {
+        //     RuleFor(x => x.yyy)
+        //         .LessThan(DateOnly.FromDateTime(DateTime.UtcNow))
+        //         .WithMessage("Date of birth must be in the past");
+        // });
+    }
+
+    private static bool HasAtLeastOneField(UpdateProfileDto dto)
+    {
+        return dto.Bio is not null;
+        // || dto.xxx is not null
+        // || dto.yyy is not null;
     }
 }
